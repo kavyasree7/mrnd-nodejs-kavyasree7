e3d850c61f71efcc8171d3e2a7d13773e398e893
SNEHAN's <SNEHAN's@SNEHAN>
2015-06-20 20:38:34 +0700

    commit changes

--- src/contacts/routes/contacts.js
+++ src/contacts/routes/contacts.js
@@ -29,18 +29,26 @@ router.post('/', function(req, res, next) {
   console.log(req.body);
   console.log(num);
   var string=JSON.stringify(req.body);
-  fs.writeFile("C:\\mrnd-nodejs-master\\spec\\tests\\data\\" + num + "-Contact.json", string, function (err) {
+  fs.writeFile("C:\\dist\\spec\\tests\\data\\" + num + "-Contact.json", string, function (err) {
         if (err) throw err;
         console.log('It\'s saved!');
   });
   res.json(num);
 });
 router.put('/:id', function(req, res, next) {
+  var ID=parseInt(req.params.id);
+  //console.log("id=");
+  //console.log(ID);
    var obj1=contacts[+(req.params.id)];//.firstName=req.body.id
    var obj2=req.body;
    for(var i in obj2)
    	obj1[i]=obj2[i];
-   //res.json(contacts[+(req.params.id)]); .firstName
+  // res.json(contacts[+(req.params.id)]); //.firstName
+   var string=JSON.stringify(obj1);
+  fs.writeFile("C:\\dist\\spec\\tests\\data\\" + ID + "-Contact.json", string, function (err) {
+        if (err) throw err;
+        console.log('in put');
+  });
    res.json(obj1);
    console.log(req.body);
 
